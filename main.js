74203ebda73a9bd6b891ca2de177f6f7e11335dc
pwcueblocks <walia11127@gmail.com>
2018-06-03 04:32:16 +0530

    variables and their vales including saving file module in progress
    Signed-off-by: pwcueblocks <walia11127@gmail.com>

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const electron = require('electron');
 const path = require('path');
 const url = require('url');
+const storage = require('electron-storage');
 
 process.env.NODE_ENV = 'development';
 
 const {app, BrowserWindow, Menu} = electron;
-
+// variables
 let mainWindow;
+
 
 app.on('ready', function(){
 
@@ -16,11 +18,19 @@ app.on('ready', function(){
         protocol: 'file:',
         slashes: true
     }));
+  
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools()
   // Build menu from template
   const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
   // Insert menu
   Menu.setApplicationMenu(mainMenu);
+
 });
+
+
+
+
 const mainMenuTemplate = [
     {
         label: 'File',
---
+++ nn.js
@@ -0,0 +1,8 @@
+// fs = require('fs');
+
+// fs.writeFile('_variables.less', 'Hello World!', function (err) {
+
+//   if (err) return console.log(err);
+//   console.log('Hello World > helloworld.txt');
+
+// });
